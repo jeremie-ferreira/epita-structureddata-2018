14f9817f8f82e2610ee3a098fe6ff303ceb3a24e
Jeremie Ferreira <jeremie.ferreira.67@gmail.com>
2018-06-29 01:22:43 +0200

    entities added

--- src/main/java/com/epita/pricer/Main.java
+++ src/main/java/com/epita/pricer/Main.java
@@ -3,9 +3,11 @@ package com.epita.pricer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
  
  
 //@Import(JpaConfiguration.class)
+@PropertySource("classpath:application.properties")
 @ImportResource("classpath:application-context.xml")
 @SpringBootApplication(scanBasePackages={"com.epita.pricer"})
 public class Main {
