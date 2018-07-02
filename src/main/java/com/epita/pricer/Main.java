20a9149490ad2c0c6f14a9ef4791408dfd068d08
Jeremie Ferreira <jeremie.ferreira.67@gmail.com>
2018-07-02 21:47:49 +0200

    fixed configuration error

--- src/main/java/com/epita/pricer/Main.java
+++ src/main/java/com/epita/pricer/Main.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
- 
- 
+
 @PropertySource("classpath:application.properties")
+@ImportResource("classpath:application-context.xml")
 @SpringBootApplication(scanBasePackages={"com.epita.pricer"})
 public class Main {
  
